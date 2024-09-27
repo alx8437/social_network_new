f0f3bd8ce7a87268cd985575d62d1fe2558c7c44
Aleks Zelenskiy <dezion777@gmail.com>
2024-09-27 16:51:07 +0300

    14. css modules was added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import styles from "./Navbar.module.css"
 
 
 const Navbar = () => {
     return (
-        <nav className="nav">
-            <div><a>Profile</a></div>
-            <div><a>Messages</a></div>
-            <div><a>News</a></div>
-            <div><a>Music</a></div>
-            <div><a>Settings</a></div>
+        <nav className={styles.nav}>
+            <div className={styles.item}><a>Profile</a></div>
+            <div className={styles.item}><a>Messages</a></div>
+            <div className={styles.item}><a>News</a></div>
+            <div className={styles.item}><a>Music</a></div>
+            <div className={styles.item}><a>Settings</a></div>
         </nav>
     )
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,16 @@
-import content from "../assets/images/content.jpg";
 import React from "react";
+import content from "../assets/images/content.jpg";
+import styles from './Profile.module.css'
+
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={styles.content}>
         <div><img src={content} alt="content"/></div>
         <div>ava + description</div>
         <div>My posts
             <div>New post</div>
-            <div>post1</div>
-            <div>post2</div>
+            <div className={styles.item}>post1</div>
+            <div className={styles.item}>post2</div>
         </div>
     </div>
 }
