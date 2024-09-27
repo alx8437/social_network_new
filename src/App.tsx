7a1629499f2e695a3fd2c40d993819dffdcdfb35
Aleks Zelenskiy <dezion777@gmail.com>
2024-09-27 10:11:23 +0300

    11 makeup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
+import logo from './assets/images/logo.png'
+import content from './assets/images/content.jpg'
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies />
+        <div className="app-wrapper">
+            <header className="header">
+                <img src={logo} alt="logo"/>
+            </header>
+            <nav className="nav">
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div><img src={content} alt="content"/></div>
+                <div>ava + description</div>
+                <div>My posts
+                    <div>New post</div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
         </div>
     );
 }
