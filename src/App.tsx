2c2cfc72dd223b413398152a8f0a3c103507cb9e
Aleks Zelenskiy <dezion777@gmail.com>
2024-10-08 16:49:51 +0300

    19. Router was added

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-{/*            <Profile />*/}
+            <Header />
+            <Navbar />
             <div className="app-wrapper-content">
-                <Dialogs/>
+                    <Routes>
+                        <Route path='/dialogs' element={<Dialogs/>} />
+                        <Route path='/profile' element={<Profile/>} />
+                    </Routes>
             </div>
         </div>
     );
