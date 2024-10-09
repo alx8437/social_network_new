4eac602a0891d8bc0c721fef9303d16213136e19
Aleks Zelenskiy <dezion777@gmail.com>
2024-10-09 16:42:07 +0300

    20. Navlinks were added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import {NavLink} from "react-router-dom";
 import styles from "./Navbar.module.css"
 
 
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
-            <div className={styles.item}><a href='/profile'>Profile</a></div>
-            <div className={styles.item}><a href='/dialogs'>Messages</a></div>
+            <div className={styles.item}><NavLink className={({isActive}) => isActive ? `${styles.activeLink}` : ""
+            } to='/profile'>Profile</NavLink></div>
+            <div className={styles.item}><NavLink className={({isActive}) =>
+                isActive ? `${styles.activeLink}` : ""
+            } to='/dialogs'>Messages</NavLink></div>
             <div className={styles.item}><a>News</a></div>
             <div className={styles.item}><a>Music</a></div>
             <div className={styles.item}><a>Settings</a></div>
