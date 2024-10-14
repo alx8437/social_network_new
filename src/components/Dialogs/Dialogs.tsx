a9421c5baffa01efce8a6ebaec94df61c9441c95
Aleks Zelenskiy <dezion777@gmail.com>
2024-10-14 15:54:11 +0300

    21. Dialogs makeup

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,20 @@
+import styles from './Dialogs.module.css'
+
 const Dialogs = (props: any) => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog + ' ' + styles.active}>Саша</div>
+                <div className={styles.dialog}>Дима</div>
+                <div className={styles.dialog}>Сергей</div>
+                <div className={styles.dialog}>Никита</div>
+                <div className={styles.dialog}>Андрей</div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi</div>
+                <div className={styles.message}>How are you?</div>
+                <div className={styles.message}>I'm fine!</div>
+            </div>
         </div>
     );
 };
